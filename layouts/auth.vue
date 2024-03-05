399845410f9b376879914ea88376cde0de48eb8c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-05 18:58:27 +0400

    created auth layout component

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useDark } from "@vueuse/core";
+const isDark = useDark();
+useHead({
+  bodyAttrs: {
+    class: "bg-secondary",
+  },
+});
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <slot />
+  </div>
+</template>
