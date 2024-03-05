7f3e8ad6cc095fbbe0c18f39b880141263c6aa16
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-05 19:00:14 +0400

    added app component

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ModalsContainer from "@/components/modals/ModalsContainer.vue";
+useHead({
+  bodyAttrs: {
+    class: "bg-white dark:bg-[#313338]",
+  },
+});
+import Toaster from "@/components/ui/toast/Toaster.vue";
+</script>
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <Toaster />
+  <ModalsContainer />
+</template>
