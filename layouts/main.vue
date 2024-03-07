7f2dc399d4a99364a547fdb7f2baa93491dcff12
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-07 19:30:23 +0400

    created main layout

---
+++ layouts/main.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import NavigationSidebar from "@/components/navigation/NavigationSidebar.vue";
+import type { Server } from "@/types/index";
+
+import { useMainStore } from "@/stores/main";
+
+const { profile } = storeToRefs(useMainStore());
+
+const { updateProfile } = useMainStore();
+
+interface ServerSidebarProps {
+  server: Server;
+}
+
+if (profile.value === null) {
+  const response = await $fetch("/api/profile");
+
+  if (response && typeof response === "object" && "id" in response) {
+    updateProfile(response);
+  }
+}
+</script>
+
+<template>
+  <ClientOnly><Setup /></ClientOnly>
+
+  <div class="h-full">
+    <div class="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+      <NavigationSidebar />
+    </div>
+    <main class="md:pl-[72px] h-full">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
