d5ea8e07caaea88266633b8b9992e08affb45958
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-07 19:31:35 +0400

    added index page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "Discord",
+});
+definePageMeta({
+  middleware: "auth",
+});
+</script>
+<template>
+  <Setup />
+  <div class="h-full p-4 space-y-2"></div>
+</template>
