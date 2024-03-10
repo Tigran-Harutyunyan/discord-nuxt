c9609ea4eb049757e074836e5309b2ddcd254946
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-10 12:08:28 +0400

    added server to main store

--- stores/main.ts
+++ stores/main.ts
@@ -1,15 +1,23 @@
 import { defineStore } from "pinia";
-import { type Profile } from "@prisma/client";
+import type { Profile } from "@prisma/client";
+import { type FullServer } from "@/types/index"
 
 export const useMainStore = defineStore("main", () => {
   const profile = ref<Profile | null>(null);
+  const server = ref<FullServer | null>(null);
 
   function updateProfile(payload: Profile) {
     profile.value = payload;
   }
 
+  function updateServer(payload: FullServer) {
+    server.value = payload;
+  }
+
   return {
     profile,
+    server,
     updateProfile,
+    updateServer
   };
 });
