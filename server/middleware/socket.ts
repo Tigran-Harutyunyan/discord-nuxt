937c7a9648440fc5c6b1661fd595d763a6b445f3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-10 12:05:01 +0400

    created socket server middleware

---
+++ server/middleware/socket.ts
@@ -0,0 +1,23 @@
+import { Socket, Server } from 'socket.io'
+
+let appSocket = {
+    emit: (channel: string, message: string) => {
+        console.log('Not initiated yet', channel, message)
+    }
+}
+
+export default defineEventHandler((event) => {
+    event.context.appSocket = appSocket
+
+    if (global.io) return
+    const node = event.node
+    global.io = new Server(node.res.socket?.server)
+    global.io.on('connection', (socket: Socket) => {
+        appSocket.emit = (channel, message) => {
+            global.io.emit(channel, message)
+        }
+
+        socket.on('disconnect', () => {
+        });
+    })
+})
