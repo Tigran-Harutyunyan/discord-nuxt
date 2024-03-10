7be0dd800dc50e13ac975e77c4770f54c47ab813
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-10 13:27:19 +0400

    created socket indicator component

---
+++ components/SocketIndicator.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Badge } from "@/components/ui/badge";
+import { useSocketStore } from "@/stores/socket";
+
+const { isConnected } = storeToRefs(useSocketStore());
+</script>
+
+<template>
+  <Badge
+    v-if="isConnected"
+    variant="outline"
+    class="bg-emerald-600 text-white border-none"
+  >
+    Live: Real-time updates
+  </Badge>
+
+  <Badge v-else variant="outline" class="bg-yellow-600 text-white border-none">
+    Fallback: Polling every 1s
+  </Badge>
+</template>
