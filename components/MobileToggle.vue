9f8de4e453ef172673c9e739dfa8d9474805b43d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-11 13:16:15 +0400

    created mobile toggle component

---
+++ components/MobileToggle.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Menu } from "lucide-vue-next";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
+import NavigationSidebar from "@/components/navigation/NavigationSidebar.vue";
+import ServerSidebar from "@/components/server/ServerSidebar.vue";
+import { useMainStore } from "@/stores/main";
+
+const { server } = storeToRefs(useMainStore());
+
+const { serverId } = defineProps<{
+  serverId: string;
+}>();
+</script>
+
+<template>
+  <Sheet>
+    <SheetTrigger>
+      <Button variant="ghost" size="icon" class="md:hidden">
+        <Menu />
+      </Button>
+    </SheetTrigger>
+    <SheetContent side="left" class="p-0 flex gap-0">
+      <div class="w-[72px]">
+        <NavigationSidebar />
+      </div>
+      <ServerSidebar :serverId="serverId" :server="server" class="pr-8" />
+    </SheetContent>
+  </Sheet>
+</template>
