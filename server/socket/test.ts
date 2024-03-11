21073e7b0e4ab7631af2cdf1c777ad39f07f20d2
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-11 13:31:29 +0400

    deleted test endpoint

--- server/socket/test.ts
+++
@@ -1,7 +0,0 @@
-import { defineIOHandler } from '../../runtime/helpers'
-
-export default defineIOHandler((io) => {
-    io.on('connection', (socket) => {
-        console.log('Connected ', socket.id)
-    })
-})
