82cf1cfd8936f4aa8e59756d0aa1921ba9969de4
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-12 15:40:52 +0400

    added some animation

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,20 @@ definePageMeta({
 </script>
 <template>
   <Setup />
-  <div class="h-full p-4 space-y-2"></div>
+  <div class="h-full p-4 space-y-2">
+    <div
+      class="flex space-x-2 justify-center items-center h-screen dark:invert"
+    >
+      <span class="sr-only">Loading...</span>
+      <div
+        class="h-8 w-8 bg-black :dark:bg-white rounded-full animate-bounce [animation-delay:-0.3s]"
+      ></div>
+      <div
+        class="h-8 w-8 bg-black :dark:bg-white rounded-full animate-bounce [animation-delay:-0.15s]"
+      ></div>
+      <div
+        class="h-8 w-8 bg-black :dark:bg-white rounded-full animate-bounce"
+      ></div>
+    </div>
+  </div>
 </template>
